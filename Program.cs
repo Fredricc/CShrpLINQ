acac0a647a7fa073834f0e754255e7662d79d42b
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-24 19:45:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace C_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 3, 44, 66, 45, 88};
+
+            var query = 
+                from n in numbers
+                where n % 2 == 0
+                select n;
+
+            foreach ( var n in query )
+            {
+                Console.WriteLine( n );
+            }
+
+        }
+    }
+}
