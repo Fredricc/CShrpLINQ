5f7b400732b35b655ebb1ae1e41816ae607c0a1e
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-25 05:34:56 +0300

    Adding Comment to show how LINQ works.

--- Program.cs
+++ Program.cs
@@ -6,13 +6,17 @@ namespace C_LINQ
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Using LINQ");
+            //Step 1: Getting the data source
             List<int> numbers = new List<int> { 1, 3, 44, 66, 45, 88};
 
+            //Step 2: Writting query
             var query = 
-                from n in numbers
-                where n % 2 == 0
-                select n;
+                from n in numbers// Part 1: Data source
+                where n % 2 == 0// Part 2: Filter
+                select n; // Part 3: Select
 
+            //Step 3: Executing query
             foreach ( var n in query )
             {
                 Console.WriteLine( n );
